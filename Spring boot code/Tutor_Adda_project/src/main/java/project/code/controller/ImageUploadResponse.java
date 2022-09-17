39dbbe8c5adff137c7ef3e900fb11be67a19f39e
karishmapatil18 <karishmapatil185@gmail.com>
2022-09-17 18:47:33 +0530

    some changes

---
+++ Spring boot code/Tutor_Adda_project/src/main/java/project/code/controller/ImageUploadResponse.java
@@ -0,0 +1,17 @@
+package project.code.controller;
+
+public class ImageUploadResponse {
+    private String message;
+
+    public ImageUploadResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
