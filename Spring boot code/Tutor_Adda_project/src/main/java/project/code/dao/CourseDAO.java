cc85dfa07d2a9b82995190ab981288cdc8cbac72
karishmapatil18 <karishmapatil185@gmail.com>
2022-09-22 11:46:31 +0530

    adding comments

--- Spring boot code/Tutor_Adda_project/src/main/java/project/code/dao/CourseDAO.java
+++ Spring boot code/Tutor_Adda_project/src/main/java/project/code/dao/CourseDAO.java
@@ -2,6 +2,7 @@ package project.code.dao;
 
 
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -21,28 +22,31 @@ public class CourseDAO  {
 	@Autowired
 	CoursesRepository coursesRepository;
 
-	public List<Courses> getAll() {
+	public List<Courses> getAll() //list of all courses
+	{ 
 		List<Courses> tlist;
 		tlist=coursesRepository.findAll();
 		return tlist;
 	}
 
-	public Courses get(int id) {
+	public Courses get(int id)  //get course details for particular course
+	{
 		return (coursesRepository.findById(id).get());
 	}
 
 
-	public Courses addcourse(Courses course ) {
-
+	public Courses addcourse(Courses course ) //add new course
+	{
 		coursesRepository.save(course);
 		return  course;
 	}
 
-	public void deletecourse(int id) {
+	public void deletecourse(int id) //delete courses by course id
+	{
 		coursesRepository.deleteById(id);
 	}
 
-	public Courses updatecourse(Courses course)
+	public Courses updatecourse(Courses course)  //update course details
 	{
 		Courses updatecourse = coursesRepository.findById(course.getCourse_id()).get();
 
@@ -61,59 +65,68 @@ public class CourseDAO  {
 		return updatecourse;
 	}
 
-	//course by teacher id
-	public List<Courses> getByTeacherId(int id) {
+	
+	public List<Courses> getByTeacherId(int id) //get course by teacher id
+	{
 		List<Courses> tlist;
 		tlist=coursesRepository.findAll().stream().filter((co)->co.getCourse_teacher_id()==id).collect(Collectors.toList()); ;
 		return tlist;
 	}
 
-	public Courses updatecoursefee(int id, int fee) {
+	public Courses updatecoursefee(int id, int fee) //Update the course fees
+	{
 		Courses c =coursesRepository.findById(id).get();
 		c.setCourse_fee(fee);
 		coursesRepository.save(c);
 		return c;
 	}
 
-	public int getcount() {
+	public int getcount()  //get count of all registered courses
+	{ 
 		int n=coursesRepository.coursecount();
 		return n;
 	}
 
-	public List<Courses> coursebydate(String date ,int tid) {
-
+	public List<Courses> coursebydate(String date ,int tid) //get all available courses for particular date
+	{
 		List<Courses> lcourse ;
-
 		lcourse=coursesRepository.coursebydate(date ,tid);
 		return lcourse;
 	}
 
-	public List<Courses> coursebydateforstudent(String date ,int sid) {
-
+	public List<Courses> coursebydateforstudent(String date ,int sid)  //List of courses for particular student for particular date
+	{
 		List<Courses> lcourse ;
-
 		lcourse=coursesRepository.coursebydatestudent(date ,sid);
 		return lcourse;
 	}
+
 	
-	public boolean verifynewcourse(Date startdate,Date enddate,String starttime,String endtime ,int tid) {
-		
+	//verify available time slot 
+	public boolean verifynewcourse(Date startdate,Date enddate,String starttime,String endtime ,int tid) 
+	{
 		List<Courses> lcourse ;
-
 		lcourse=coursesRepository.verifynewcourse(startdate,enddate,starttime,endtime ,tid);
-
 		if(lcourse.isEmpty())
-			{return true;}
+		{return true;}
 		else
 			return false;
-		
+
 	}
 
-	public Courses teacherupcommingcourse(int tid) {
+	public Courses teacherupcommingcourse(int tid) //gives upcoming one course lecture for particular teacher 
+	{
 		Courses course;
 		course=coursesRepository.upcomming_course_teahcer(tid);
-		
+
 		return course;
+	}
+
+	public List<Courses> coursesbystudentid(int id)  //get all courses by student id
+	{
+		List<Courses>courses = new ArrayList<Courses>();
+		courses =coursesRepository.getcoursebystudentid(id);
+		return courses;
 	}
 
 }
